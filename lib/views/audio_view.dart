a1264e03fba396b3d4e5fbf1f40c6dfa58d7da27
abdoulahi21 <abdoulahi.diallo@unchk.edu.sn>
2025-02-13 13:55:41 +0000

    model translation

---
+++ lib/views/audio_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class AudioView extends StatefulWidget {
+  const AudioView({super.key});
+
+  @override
+  State<AudioView> createState() => _AudioViewState();
+}
+
+class _AudioViewState extends State<AudioView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
